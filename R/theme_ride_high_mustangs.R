0f420195ee95f2799be8ee9810acbbb8c0e75fd8
Chaicow <100246644+Chaicow@users.noreply.github.com>
2022-04-24 17:05:11 -0700

    Edited the theme_ride_high_mustangs() function

--- R/theme_ride_high_mustangs.R
+++ R/theme_ride_high_mustangs.R
@@ -5,22 +5,20 @@
 #' @param text_color color of the text
 #' @param title_color color of the title
 #' @param background_color color of plot's background
-#' @param line_color line color
 #' @importFrom ggplot2 theme
 #' @export
 
 theme_ride_high_mustangs <- function(plot, title_font= "Trebuchet MS Bold",
                      text_font = "Trebuchet MS",
-                     text_color = "#143F2E",
-                     title_color = "#358639",
-                     line_color = "#F2C75D",
-                     background_color = "#FFFFFF") {
+                     text_color = "#F2C75D",
+                     title_color = "#143F2E",
+                     background_color = "ivory") {
 plot +
+  theme_bw() +
   theme(text = element_text(family = text_font,
                         color = text_color),
     title = element_text(family = title_font,
                          color = title_color),
-    line = element_line(color = line_color),
-    plot.background = element_rect(fill = background_color),
+    panel.background = element_rect(fill = background_color),
     legend.position = "none")
 }
