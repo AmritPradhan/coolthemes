32e527b909c90dc20ac73c50252de0128cf4a41b
hasiegler <hasiegler@gmail.com>
2022-04-25 18:46:37 -0700

    changed to plot + theme_ride style

--- R/theme_ride_high_mustangs.R
+++ R/theme_ride_high_mustangs.R
@@ -1,24 +1,26 @@
 #' A custom theme for ggplot2 visualizations based off of Cal Poly! Ride High Mustangs
+#'
 #' @param plot plot that theme will be applied to
 #' @param title_font font for title
 #' @param text_font font for text
 #' @param text_color color of the text
 #' @param title_color color of the title
 #' @param background_color color of plot's background
+#'
 #' @importFrom ggplot2 theme
+#'
 #' @export
 
-theme_ride_high_mustangs <- function(plot, title_font= "Trebuchet MS Bold",
+theme_ride_high_mustangs <- function(title_font= "Trebuchet MS Bold",
                      text_font = "Trebuchet MS",
                      text_color = "#F2C75D",
                      title_color = "#143F2E",
                      background_color = "ivory") {
-plot +
   theme_bw() +
   theme(text = element_text(family = text_font,
-                        color = text_color),
-    title = element_text(family = title_font,
-                         color = title_color),
-    panel.background = element_rect(fill = background_color),
-    legend.position = "none")
+                            color = text_color),
+        plot.title = element_text(family = title_font,
+                                  color = title_color),
+        panel.background = element_rect(fill = background_color),
+        legend.position = "none")
 }
