61e729e699de0f029c8fc71d8b6c7f3dedf934ef
Chaicow <100246644+Chaicow@users.noreply.github.com>
2022-04-25 21:40:50 -0700

    Added a helper function to CP theme

--- R/theme_ride_high_mustangs.R
+++ R/theme_ride_high_mustangs.R
@@ -1,15 +1,36 @@
 #' A custom theme for ggplot2 visualizations based off of Cal Poly! Ride High Mustangs
-#' @param plot plot that theme will be applied to
+#' @param title_font font for title
+#' @param text_font font for text
+#' @param text_color color of the text
+#' @param title_color color of the title
+#' @param background_color color of plot's background
+#'
+#' @importFrom ggplot2 theme
+#'
+#' @export
+
+theme_ride_high_mustangs <- function(title_font= "Trebuchet MS Bold",
+                                     text_font = "Trebuchet MS",
+                                     text_color = "#F2C75D",
+                                     title_color = "#143F2E",
+                                     background_color = "ivory") {
+  theme_bw() +
+    theme(text = element_text(family = text_font,
+                              color = text_color),
+          plot.title = element_text(family = title_font,
+                                    color = title_color),
+          panel.background = element_rect(fill = background_color),
+          legend.position = "none")
+}
+
+
+#' This is a helper function that allows users to try out the Cal Poly Theme!
+#' @param plot Plot that you want to test the Cal Poly theme on!
 #' @importFrom ggplot2 theme
 #' @export
 
-theme_ride_high_mustangs <- function(plot) {
-plot +
-  theme_bw() +
-  theme(text = element_text(family = "Trebuchet MS",
-                        color = "#F2C75D"),
-    title = element_text(family = "Trebuchet MS Bold",
-                         color = "#143F2E"),
-    panel.background = element_rect(fill = "ivory"),
-    legend.position = "none")
+try_theme_ride_high_mustangs <- function(plot) {
+  plot +
+    theme_ride_high_mustangs()
+
 }
--- tests/testthat/test-theme_ride_high_mustangs.R
+++ tests/testthat/test-theme_ride_high_mustangs.R
@@ -1,11 +1,39 @@
 test_that("theme_ride_high_mustangs works", {
-library(ggplot2)
+  library(ggplot2)
+
   test_plot <- iris %>%
     ggplot(mapping = aes(x = Sepal.Length, y = Sepal.Width)) +
+    geom_point() +
+    ggtitle("test")
+
+  correct_result <- test_plot +
+    theme_bw() +
+    theme(text = element_text(family = "Trebuchet MS",
+                              color = "#F2C75D"),
+          plot.title = element_text(family = "Trebuchet MS Bold",
+                                    color = "#143F2E"),
+          panel.background = element_rect(fill = "ivory"),
+          legend.position = "none")
+
+  color_correct <- correct_result$theme$text$colour
+
+  res <- test_plot +
+    theme_ride_high_mustangs()
+
+  color_res <- res$theme$text$colour
+
+  expect_equal(color_res, color_correct)
+
+})
+
+
+test_that("try_theme_ride_high_mustangs works", {
+  try_plot <- iris %>%
+    ggplot(mapping = aes(y = Sepal.Length, x = Sepal.Width)) +
     geom_line()+
     ggtitle("test")
 
-  test_plot +
+  correct_result <- try_plot +
     theme_bw() +
     theme(text = element_text(family = 'Trebuchet MS',
                               color = "#F2C75D"),
@@ -13,10 +41,12 @@ library(ggplot2)
                                     color = "#143F2E"),
           panel.background = element_rect(fill = "ivory"),
           legend.position = "none")
+  correct_try <- correct_result$theme$text$colour
 
-  my_result <- theme_ride_high_mustangs(test_plot)
+  my_result <- try_theme_ride_high_mustangs(try_plot)
+  color_try <- my_result$theme$text$colour
 
-  correct_result <- theme_ride_high_mustangs(test_plot)
+  expect_equal(color_try, correct_try)
 
-  expect_equal(my_result, correct_result)
+
 })
