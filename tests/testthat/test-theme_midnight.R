941d1a88c0c2e030fc0b988cdd7adc2d5c708384
hasiegler <hasiegler@gmail.com>
2022-04-29 23:17:29 -0700

    added all tests for theme midnight

--- tests/testthat/test-theme_midnight.R
+++ tests/testthat/test-theme_midnight.R
@@ -1,5 +1,29 @@
-test_that("theme_midnight works", {
-library(ggplot2)
+test_that("change_background_color works",{
+
+  library(ggplot2)
+
+  test_plot <- iris %>%
+    ggplot(mapping = aes(x = Sepal.Length, y = Sepal.Width)) +
+    geom_point()
+
+  my_res <- test_plot +
+    change_background_color(background_color = "blue")
+
+  correct_res <- test_plot +
+    theme(panel.background = element_rect(fill = "blue"),
+          plot.background = element_rect(fill = "blue"),
+          legend.background = element_rect(fill = "blue"),
+          legend.key = element_rect(fill = "blue"))
+
+  res_color <- my_res$theme$plot.background$fill
+
+  correct_color <- correct_res$theme$plot.background$fill
+
+  expect_equal(res_color, correct_color)
+
+})
+
+test_that("theme_midnight returns the correct text color", {
 
   test_plot <- iris %>%
     ggplot(mapping = aes(x = Sepal.Length, y = Sepal.Width)) +
@@ -23,36 +47,139 @@ library(ggplot2)
                                 vjust = 3),
       plot.subtitle = element_text(color = "wheat",
                                    size = 13),
-      plot.caption = element_text(color = "wheat")
+      plot.caption = element_text(color = "wheat"),
+
+      panel.background = element_rect(fill = "gray40"),
+      plot.background = element_rect(fill = "gray40"),
+      legend.background = element_rect(fill = "gray40"),
+      legend.key = element_rect(fill = "gray40")
     )
 
-  res_text_color <- my_res$theme$text$colour
+  res <- my_res$theme$text$colour
 
-  correct_text_color <- correct_res$theme$text$colour
+  correct <- correct_res$theme$text$colour
 
-  expect_equal(res_text_color, correct_text_color)
+  expect_equal(res, correct)
 
 })
 
-test_that("change_background_color works",{
+test_that("theme_midnight returns the correct title color", {
 
   test_plot <- iris %>%
     ggplot(mapping = aes(x = Sepal.Length, y = Sepal.Width)) +
     geom_point()
 
   my_res <- test_plot +
-    change_background_color(background_color = "blue")
+    theme_midnight()
 
   correct_res <- test_plot +
-    theme(panel.background = element_rect(fill = "blue"),
-          plot.background = element_rect(fill = "blue"),
-          legend.background = element_rect(fill = "blue"),
-          legend.key = element_rect(fill = "blue"))
+    theme(
+      panel.grid.minor = element_line(color = "floralwhite"),
+      panel.grid.major = element_line(color = "floralwhite"),
 
-  res_color <- my_res$theme$plot.background$fill
+      text = element_text(color = "wheat"),
+      axis.text = element_text(color = "wheat"),
 
-  correct_color <- correct_res$theme$plot.background$fill
+      plot.title = element_text(color = "wheat",
+                                size = 19,
+                                face = "bold",
+                                hjust = 0,
+                                vjust = 3),
+      plot.subtitle = element_text(color = "wheat",
+                                   size = 13),
+      plot.caption = element_text(color = "wheat"),
 
-  expect_equal(res_color, correct_color)
+      panel.background = element_rect(fill = "gray40"),
+      plot.background = element_rect(fill = "gray40"),
+      legend.background = element_rect(fill = "gray40"),
+      legend.key = element_rect(fill = "gray40")
+    )
+
+  res <- my_res$theme$plot.title$colour
+
+  correct <- correct_res$theme$plot.title$colour
+
+  expect_equal(res, correct)
+
+})
+
+test_that("theme_midnight returns the correct background color", {
+
+  test_plot <- iris %>%
+    ggplot(mapping = aes(x = Sepal.Length, y = Sepal.Width)) +
+    geom_point()
+
+  my_res <- test_plot +
+    theme_midnight(background_color = "orange")
+
+  correct_res <- test_plot +
+    theme(
+      panel.grid.minor = element_line(color = "floralwhite"),
+      panel.grid.major = element_line(color = "floralwhite"),
+
+      text = element_text(color = "wheat"),
+      axis.text = element_text(color = "wheat"),
+
+      plot.title = element_text(color = "wheat",
+                                size = 19,
+                                face = "bold",
+                                hjust = 0,
+                                vjust = 3),
+      plot.subtitle = element_text(color = "wheat",
+                                   size = 13),
+      plot.caption = element_text(color = "wheat"),
+
+      panel.background = element_rect(fill = "orange"),
+      plot.background = element_rect(fill = "orange"),
+      legend.background = element_rect(fill = "oragne"),
+      legend.key = element_rect(fill = "oragne")
+    )
+
+  res <- my_res$theme$plot.background$colour
+
+  correct <- correct_res$theme$plot.background$colour
+
+  expect_equal(res, correct)
+
+})
+
+
+test_that("theme_midnight returns the grid line color", {
+
+  test_plot <- iris %>%
+    ggplot(mapping = aes(x = Sepal.Length, y = Sepal.Width)) +
+    geom_point()
+
+  my_res <- test_plot +
+    theme_midnight()
+
+  correct_res <- test_plot +
+    theme(
+      panel.grid.minor = element_line(color = "floralwhite"),
+      panel.grid.major = element_line(color = "floralwhite"),
+
+      text = element_text(color = "wheat"),
+      axis.text = element_text(color = "wheat"),
+
+      plot.title = element_text(color = "wheat",
+                                size = 19,
+                                face = "bold",
+                                hjust = 0,
+                                vjust = 3),
+      plot.subtitle = element_text(color = "wheat",
+                                   size = 13),
+      plot.caption = element_text(color = "wheat"),
+
+      panel.background = element_rect(fill = "gray40"),
+      plot.background = element_rect(fill = "gray40"),
+      legend.background = element_rect(fill = "gray40"),
+      legend.key = element_rect(fill = "gray40")
+    )
+
+  res <- my_res$theme$panel.grid.major$colour
+
+  correct <- correct_res$theme$panel.grid.major$colour
+
+  expect_equal(res, correct)
 
 })
